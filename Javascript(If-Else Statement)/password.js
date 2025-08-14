66395f62fb0c4522a675b1590a02266fda8dec3a
8164Smit <8164smitvasava@gmail.com>
2025-08-14 15:51:02 +0530

    Update password.js

--- Javascript(If-Else Statement)/password.js
+++ Javascript(If-Else Statement)/password.js
@@ -6,15 +6,9 @@ if (oldpassword === setpassword) {
     var newpassword = prompt("Enter New Password: ");
     var confirmpassword = prompt("Enter Confirm Password: ");
     if (newpassword === confirmpassword) {
-        // newpassword = confi;
         console.log(`Your New Password ${newpassword} is setted successfully...`);
     }
     else {
-        console.log("Incorrect Password Entered....");
+        console.log("Passwords do not match.");
     }
 }
-
-// Pela j password chee aa levanu
-// Compare karvanu aj che k naii, hoi to new password ni permission apvani nai to nai
-// Password match thaya pachi new password enter karavanu ,ane ene confirm password jode match karavanu
-// Banne match thai to message pass karai devanu k new password setted successfully
